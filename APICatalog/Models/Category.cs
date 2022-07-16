681ede31261a4f6b3ec711f889ea36bae73131c0
Laiane Muckler <laianecm@gmail.com>
2022-07-15 22:03:06 -0300

    first commit

---
+++ APICatalog/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace APICatalog.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string? Name { get; set; }
+        public string? UrlImage{ get; set; }
+    }
+}
---
+++ APICatalog/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace APICatalog.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string UrlImage { get; set; }
+        public float Inventory { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+    }
+}
